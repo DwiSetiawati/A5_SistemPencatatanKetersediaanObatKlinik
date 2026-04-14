4094c1625698df8e735e9800c160fa84cdedaf23
DwiSetiawati <dwisetwa27@gmail.com>
2026-04-15 06:51:12 +0700

    Add project files.

---
+++ ObatKlinikADO/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace ObatKlinikADO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtSatuan = new System.Windows.Forms.TextBox();
+            this.txtStok = new System.Windows.Forms.TextBox();
+            this.txtCari = new System.Windows.Forms.TextBox();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnTampilkan = new System.Windows.Forms.Button();
+            this.btnCari = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvObat = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvObat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(129, 39);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(125, 22);
+            this.txtID.TabIndex = 0;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(129, 76);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(125, 22);
+            this.txtNama.TabIndex = 1;
+            // 
+            // txtSatuan
+            // 
+            this.txtSatuan.Location = new System.Drawing.Point(129, 116);
+            this.txtSatuan.Name = "txtSatuan";
+            this.txtSatuan.Size = new System.Drawing.Size(125, 22);
+            this.txtSatuan.TabIndex = 2;
+            // 
+            // txtStok
+            // 
+            this.txtStok.Location = new System.Drawing.Point(129, 157);
+            this.txtStok.Name = "txtStok";
+            this.txtStok.Size = new System.Drawing.Size(125, 22);
+            this.txtStok.TabIndex = 3;
+            // 
+            // txtCari
+            // 
+            this.txtCari.Location = new System.Drawing.Point(129, 206);
+            this.txtCari.Name = "txtCari";
+            this.txtCari.Size = new System.Drawing.Size(125, 22);
+            this.txtCari.TabIndex = 4;
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Location = new System.Drawing.Point(62, 317);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(82, 33);
+            this.btnTambah.TabIndex = 5;
+            this.btnTambah.Text = "Tambah";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(208, 317);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(85, 33);
+            this.btnHapus.TabIndex = 6;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(355, 317);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(87, 33);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnTampilkan
+            // 
+            this.btnTampilkan.Location = new System.Drawing.Point(509, 317);
+            this.btnTampilkan.Name = "btnTampilkan";
+            this.btnTampilkan.Size = new System.Drawing.Size(88, 33);
+            this.btnTampilkan.TabIndex = 8;
+            this.btnTampilkan.Text = "Tampilkan";
+            this.btnTampilkan.UseVisualStyleBackColor = true;
+            // 
+            // btnCari
+            // 
+            this.btnCari.Location = new System.Drawing.Point(650, 317);
+            this.btnCari.Name = "btnCari";
+            this.btnCari.Size = new System.Drawing.Size(85, 33);
+            this.btnCari.TabIndex = 9;
+            this.btnCari.Text = "Cari";
+            this.btnCari.UseVisualStyleBackColor = true;
+            this.btnCari.Click += new System.EventHandler(this.btnCari_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "ID Obat : ";
+            // 
+            // dgvObat
+            // 
+            this.dgvObat.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvObat.Location = new System.Drawing.Point(416, 39);
+            this.dgvObat.Name = "dgvObat";
+            this.dgvObat.RowHeadersWidth = 51;
+            this.dgvObat.RowTemplate.Height = 24;
+            this.dgvObat.Size = new System.Drawing.Size(356, 189);
+            this.dgvObat.TabIndex = 11;
+            this.dgvObat.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvObat_CellClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Nama Obat :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Satuan :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Stok Obat :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Cari Obat";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(422, 45);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(47, 16);
+            this.lblTotal.TabIndex = 16;
+            this.lblTotal.Text = "Hasil : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvObat);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCari);
+            this.Controls.Add(this.btnTampilkan);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.txtCari);
+            this.Controls.Add(this.txtStok);
+            this.Controls.Add(this.txtSatuan);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtID);
+            this.Name = "Form1";
+            this.Text = "Sistem Pencatatan Obat Klinik";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvObat)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtSatuan;
+        private System.Windows.Forms.TextBox txtStok;
+        private System.Windows.Forms.TextBox txtCari;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnTampilkan;
+        private System.Windows.Forms.Button btnCari;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvObat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
