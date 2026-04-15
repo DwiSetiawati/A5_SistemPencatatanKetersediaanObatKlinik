f69f5be025436f426cd50bd0bb97f1c0bd2d855d
DwiSetiawati <dwisetwa27@gmail.com>
2026-04-15 07:11:01 +0700

    Implementasi logika autentikasi login dan pengecekan role

--- ObatKlinikADO/Form2.cs
+++ ObatKlinikADO/Form2.cs
@@ -24,6 +24,32 @@ namespace ObatKlinikADO
 
         }
 
-      
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                // Mengambil role untuk dikirim ke form utama
+                SqlCommand cmd = new SqlCommand("SELECT role FROM Akun WHERE username=@u AND password=@p", con);
+                cmd.Parameters.AddWithValue("@u", txtUsername.Text);
+                cmd.Parameters.AddWithValue("@p", txtPassword.Text);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    string role = dr["role"].ToString();
+                    MessageBox.Show("Selamat Datang, " + role);
+
+                    // Buka Form Utama dan kirim Role-nya
+                    Form1 utama = new Form1(role);
+                    utama.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Username/Password Salah!");
+                }
+            }
+        }
     }
 }
